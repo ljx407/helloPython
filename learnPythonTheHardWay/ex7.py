e2105b2021e6ee86fac390e8a1c35115a64c4912
= <=>
2016-07-11 13:17:33 +0800

    add file ex7

---
+++ learnPythonTheHardWay/ex7.py
@@ -0,0 +1,40 @@
+# print a string
+print "Mary had a little lamp."
+# print a string
+print "Its fleese was white as %s." % 'snow'
+# print a string
+print "And everywhere that Mary went."
+# circulate print the string for 10 times
+print "." * 10 # what'd that do
+# print "Hello" * 20
+
+# define a variable named end1
+end1 = "C"
+# define a variable named end2
+end2 = "h"
+# define a variable named end2
+end3 = "e"
+# define a variable named end3
+end4 = "e"
+# define a variable named end4
+end5 = "s"
+# define a variable named end5
+end6 = "e"
+# define a variable named end6
+end7 = "B"
+# define a variable named end7
+end8 = "u"
+# define a variable named end8
+end9 = "r"
+# define a variable named end9
+end10 = "g"
+# define a variable named end10
+end11 = "e"
+# define a variable named end11
+end12 = "r"
+# define a variable named end12
+
+# watch that comma at the end . try removing it to see what happen
+#if add a comma at the end ,then the two string will print in one line , otherwise print in two line
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
